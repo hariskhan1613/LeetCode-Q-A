c68abf699c2b7694d6d29b56a5395d77192ca1f4
Mohd Haris Khan <85868037+hariskhan1613@users.noreply.github.com>
2026-09-17 21:07:14 +0530

    Time: 1 ms (100%), Space: 45.3 MB (81.98%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,32 +1,26 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        Boolean[][] dp = new Boolean[s.length() + 1][p.length() + 1];
-        return f(s, p, 0, 0, dp);
-    }
-
-    boolean f(String s, String p, int i, int j, Boolean[][] dp) {
-
-        if (i == s.length() && j == p.length())
-            return true;
-        if (j == p.length())
-            return false;
-        if (i == s.length()) {
-            while (j < p.length()) {
-                if (p.charAt(j) != '*')
-                    return false;
-                j++;
+        int si = 0, pi = 0, match = 0, star = -1;
+        int sn = s.length(), pn = p.length();
+        while (si < sn) {
+            if (pi < pn && (p.charAt(pi) == '?' || p.charAt(pi) == s.charAt(si))) {
+                si++;
+                pi++;
+            } else if (pi < pn && p.charAt(pi) == '*') {
+                star = pi;
+                match = si;
+                pi++;
+            } else if (star != -1) {
+                pi = star + 1;
+                match++;
+                si = match;
+            } else {
+                return false;
             }
-            return true;
         }
-        if (dp[i][j] != null)
-            return dp[i][j];
-        if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?') {
-            dp[i][j] = f(s, p, i + 1, j + 1, dp);
-        } else if (p.charAt(j) == '*') {
-            dp[i][j] = f(s, p, i + 1, j, dp) || f(s, p, i, j + 1, dp);
-        } else {
-            dp[i][j] = false;
+        while (pi < pn && p.charAt(pi) == '*') {
+            pi++;
         }
-        return dp[i][j];
+        return pi == pn;
     }
 }
