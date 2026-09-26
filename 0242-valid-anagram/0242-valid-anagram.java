fd399bbec9c51b334ab08fd8fbfab3f16a455751
Mohd Haris Khan <85868037+hariskhan1613@users.noreply.github.com>
2026-09-26 18:22:49 +0530

    Time: 17 ms (16.5%), Space: 46.5 MB (24.37%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,15 +4,19 @@ class Solution {
             return false;
         }
 
-        int[] freq = new int[26];
+        HashMap<Character, Integer> one = new HashMap<>();
+        HashMap<Character, Integer> two = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
-            freq[s.charAt(i) - 'a']++;
-            freq[t.charAt(i) - 'a']--;
+            one.put(s.charAt(i), one.getOrDefault(s.charAt(i), 0) + 1);
         }
 
-        for (int count : freq) {
-            if (count != 0) {
+        for (int i = 0; i < t.length(); i++) {
+            two.put(t.charAt(i), two.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+        for (char c : one.keySet()) {
+            if (!one.get(c).equals(two.get(c))) {
                 return false;
             }
         }
